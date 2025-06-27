108eaf87ef4cf8349431c23f03e253acd986cd0c
Trent2000Steel <thomas4out@gmail.com>
2025-06-27 17:33:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ export default function Home() {
       <Head>
         <title>MovingCo — Moving You Can Trust</title>
         <meta name="description" content="Flat Rate Pricing. No Money Down. Real Human Help." />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
