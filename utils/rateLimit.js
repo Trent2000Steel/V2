6f1e904599094bef3c137a23c426e16099430ab2
Trent2000Steel <thomas4out@gmail.com>
2025-06-29 15:21:59 -0500

    Update rateLimit.js

--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -1,27 +1,22 @@
-// utils/rateLimit.js
-const LRU = require('lru-cache');
+import LRU from 'lru-cache';
 
-export default function rateLimit({ interval, uniqueTokenPerInterval }) {
+const rateLimit = (options) => {
   const tokenCache = new LRU({
-    max: uniqueTokenPerInterval || 500,
-    ttl: interval || 60000, // 1 minute default
+    max: options.uniqueTokenPerInterval || 500,
+    ttl: options.interval || 60000, // TTL in ms
   });
 
   return {
-    check: (res, limit, token) =>
-      new Promise((resolve, reject) => {
-        const tokenCount = tokenCache.get(token) || [0];
-        if (tokenCount[0] === 0) {
-          tokenCache.set(token, tokenCount);
-        }
-        tokenCount[0] += 1;
-
-        const currentUsage = tokenCount[0];
-        const isRateLimited = currentUsage > limit;
-        res.setHeader('X-RateLimit-Limit', limit);
-        res.setHeader('X-RateLimit-Remaining', Math.max(0, limit - currentUsage));
-
-        return isRateLimited ? reject() : resolve();
-      }),
+    check: (res, limit, token) => {
+      const tokenCount = tokenCache.get(token) || 0;
+      if (tokenCount >= limit) {
+        res.status(429).json({ error: 'Rate limit exceeded' });
+        return false;
+      }
+      tokenCache.set(token, tokenCount + 1);
+      return true;
+    },
   };
-}
+};
+
+export default rateLimit;
