7d7efcc75fac6b19c7ea8a3f81d748312fc16843
Trent2000Steel <thomas4out@gmail.com>
2025-06-30 12:47:32 -0500

    Update Memory.js

--- utils/Memory.js
+++ utils/Memory.js
@@ -1,9 +1,16 @@
 let memory = {
   messages: [],
   quote: null,
+  leadSent: false,
+  customerIntent: '', // 'reserve' or 'email'
   customerInfo: {
+    fullName: '',
+    email: '',
+    phone: '',
     origin: '',
-    destination: '',
+    destination: '',         // full address or general
+    destinationCity: '',
+    destinationState: '',
     moveDate: '',
     homeSize: '',
     priorities: '',
@@ -22,4 +29,16 @@ export function updateMemory(newMessage) {
 
 export function saveQuote(quote) {
   memory.quote = quote;
+}
+
+export function setCustomerInfo(partialInfo) {
+  Object.assign(memory.customerInfo, partialInfo);
+}
+
+export function setIntent(intent) {
+  memory.customerIntent = intent;
+}
+
+export function markLeadSent() {
+  memory.leadSent = true;
 }
