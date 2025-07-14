325cdc0c6d66d684ce8620662a2574f1126511fa
Trent2000Steel <thomas4out@gmail.com>
2025-07-14 14:46:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,22 @@ export default function Home() {
             `,
           }}
         />
+
+        {/* ✅ Hotjar Tracking */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:6442537,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </Head>
 
       <div style={styles.page}>
